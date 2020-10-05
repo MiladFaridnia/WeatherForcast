1ffaba1a8bcd90190bbbbf05b681a06eaf22ce1e
Milad <Milad.faridnia@gmail.com>
2020-10-05 09:45:26 +0330

    feat : Add retrofit related classes

---
+++ app/src/main/java/com/faridnia/cafe/bazaar/weatherforcast/network/ApiClient.kt
@@ -0,0 +1,36 @@
+package com.faridnia.cafe.bazaar.weatherforcast.network
+
+import com.faridnia.cafe.bazaar.weatherforcast.BuildConfig
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    private var retrofit: Retrofit? = null
+    private val gson = GsonBuilder().setLenient().create()
+
+    val client: Retrofit
+        get() {
+            if (retrofit == null) {
+                synchronized(Retrofit::class.java) {
+                    if (retrofit == null) {
+
+                        val httpClient = OkHttpClient.Builder()
+                            .addInterceptor(QueryParameterAddInterceptor())
+
+                        val client = httpClient.build()
+
+                        retrofit = Retrofit.Builder()
+                            .baseUrl(BuildConfig.BASE_URL)
+                            .addConverterFactory(GsonConverterFactory.create(gson))
+                            .client(client)
+                            .build()
+                    }
+                }
+
+            }
+            return retrofit!!
+        }
+}
--- app/src/main/java/com/faridnia/cafe/bazaar/weatherforcast/view/MainActivity.kt
+++ app/src/main/java/com/faridnia/cafe/bazaar/weatherforcast/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.faridnia.cafe.bazaar.weatherforcast
+package com.faridnia.cafe.bazaar.weatherforcast.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.faridnia.cafe.bazaar.weatherforcast.R
 
 class MainActivity : AppCompatActivity() {
 
